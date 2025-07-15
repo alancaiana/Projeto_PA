b9366406ce43fd26f56247b575b4b0777db34a60
alancaiana <alanfrancisco80@gmail.com>
2025-07-14 22:31:04 -0300

    alteração do wsgi

--- APP/wsgi.py
+++ APP/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for APP project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+path = '/home/alancaiana/mysite'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'APP.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'APP.settings')
 
 application = get_wsgi_application()
